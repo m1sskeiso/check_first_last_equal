420398dc34c7a43540aae487303e56b9a0173906
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-22 03:26:19 +0700

    first

--- check_first_last_equal.py
+++ check_first_last_equal.py
@@ -1,6 +1,12 @@
 # Defining function to check if the first and last number are equal
-# Check if the list is not empty
-# Compare the first and last elements of the list
+def check_first_last_equal(input_list):
+    # Check if the list is not empty
+    if input_list:
+        # Compare the first and last elements of the list
+        return input_list[0] == input_list[-1]
+    else:
+        # If the list is empty, return False
+        return False
 # If the list is empty, return False
 # Test cases
 
