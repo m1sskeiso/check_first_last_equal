59c187da0a1084995739bf486c32bd8790f6930c
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-22 03:22:05 +0700

    pseudocode

---
+++ check_first_last_equal.py
@@ -0,0 +1,7 @@
+# Defining function to check if the first and last number are equal
+# Check if the list is not empty
+# Compare the first and last elements of the list
+# If the list is empty, return False
+# Test cases
+
+
